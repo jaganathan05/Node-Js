9673b3b28b4626b7f34ad8ce4098c5820a5ae07a
jaganathan05 <jaganathanv888@gmail.com>
2023-08-10 21:50:08 +0530

    array , spread & rest operator

--- first-app.js
+++ first-app.js
@@ -1,10 +1,8 @@
-const productoftwonums =(a,b)=>{
-    return a*b;
-}
-console.log(productoftwonums(2,3));
-
-const student = {
+const productoftwonums =(a,b)=>{ return a*b; } 
+//console.log(productoftwonums(2,3)); 
+const student = { 
     name:'Jaganathan',
-    age:21,
-}
-console.log(student)
+    age:21, } 
+//console.log(student) 
+const array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon']; 
+console.log(array.map(arr=>arr=(arr===' ' ? 'empty string': arr)));
