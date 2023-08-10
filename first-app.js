634ddabaa8d09e8c961e11c300f3c5741a61f551
jaganathan05 <jaganathanv888@gmail.com>
2023-08-11 00:04:52 +0530

    create a node js server

--- first-app.js
+++ first-app.js
@@ -1,8 +1,7 @@
-const productoftwonums =(a,b)=>{ return a*b; } 
-//console.log(productoftwonums(2,3)); 
-const student = { 
-    name:'Jaganathan',
-    age:21, } 
-//console.log(student) 
-const array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon']; 
-console.log(array.map(arr=>arr=(arr===' ' ? 'empty string': arr)));
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log('my Name is jaganathan ')
+})
+
+server.listen(4000);
