013cbe118d4fa0e60063e17ccf5e68638b6b875b
jaganathan05 <jaganathanv888@gmail.com>
2023-08-11 10:35:26 +0530

    welcome

--- first-app.js
+++ first-app.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('my Name is jaganathan ')
+    console.log(req.url , req.method ,req.headers );
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<head><title>home</title></head>')
+    res.write('<body><h1>Welcome to my Node js project!</h1>')
+    res.write('</html>')
+    res.end('')
 })
 
 server.listen(4000);
