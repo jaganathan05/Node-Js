599250eecfceccabf503209b2e687bbf821ce0fc
jaganathan05 <jaganathanv888@gmail.com>
2023-08-11 10:36:22 +0530

    return specific url

--- first-app.js
+++ first-app.js
@@ -5,7 +5,17 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type','text/html');
     res.write('<html>')
     res.write('<head><title>home</title></head>')
-    res.write('<body><h1>Welcome to my Node js project!</h1>')
+    res.write('<body>')
+    if (req.url === '/home') {
+        res.write('<h1>Welcome home</h1>');
+    } else if (req.url === '/about') {
+        res.write('<h1>Welcome to About Us page</h1>');
+    } else if (req.url === '/node') {
+        res.write('<h1>Welcome to my Node.js project</h1>');
+    } else {
+        res.write('<h1>welcome</h1>');
+    }
+    res.write('</body>')
     res.write('</html>')
     res.end('')
 })
