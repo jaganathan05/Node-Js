fa903b8221cf1c70b296187fcfd2d3df57913be0
jaganathan05 <jaganathanv888@gmail.com>
2023-08-16 14:24:00 +0530

    express.js

--- first-app.js
+++ first-app.js
@@ -1,8 +1,15 @@
-const http = require('http');
-const routes = require('./routes');
 
-const server = http.createServer(routes.handler);
-console.log(routes.sometext)
+const express = require('express');
+const app = express();
 
+app.use((req,res,next)=>{
+    console.log('in the middleware');
+    next();
+})
+app.use((req,res,next)=>{
+    console.log('in the another middleware');
+    res.send('<h1>welcome to express.js</h1>')
+})
 
-server.listen(3000);
+app.listen(3000);
+
