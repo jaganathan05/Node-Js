625ef8e12f4230bc1e1d08f6c21200e350180b8e
jaganathan05 <jaganathanv888@gmail.com>
2023-08-17 12:20:26 +0530

    another input

--- first-app.js
+++ first-app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended : false} ));
 app.use('/add-product', (req,res,next)=>{
     console.log('in the middleware');
-    res.send('<form action="/product" method="POST" ><input type="text"  name="title"><button type="submit">add product</button> </form>')
+    res.send('<form action="/product" method="POST" ><lable>product</lable><input type="text"  name="title"><br><br><lable>size</lable><input type="number" name="size" min="1" max="10"><button type="submit">add product</button> </form>')
 })
 app.post('/product',(req,res, next) =>{
     console.log(req.body);
