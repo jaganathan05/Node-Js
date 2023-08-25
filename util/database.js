aed76d807c2c114ecbb47e6d2c0cccb36b494ccf
jaganathan05 <jaganathanv888@gmail.com>
2023-08-25 22:22:17 +0530

    reteriving data

--- util/database.js
+++ util/database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'Jaguar@05'
-})
+    user: 'root',
+    database: 'node-complete',
+    password: 'Jaguar@05'
+});
 
 module.exports = pool.promise();
