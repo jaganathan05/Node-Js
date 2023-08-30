35dd00b385e9994b58dfaab080e9c5d19a5b18f8
jaganathan05 <jaganathanv888@gmail.com>
2023-08-30 11:09:52 +0530

    Delete data in sql and retrive data

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,8 +14,11 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product.save().then(()=>{
+    res.redirect('/');
+  })
+  .catch(err=>{console.log(err)})
+  
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -24,18 +27,16 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
-    }
+  Product.findById(prodId).then(()=>{
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
       product: product
-    });
-  });
-};
+  })
+})
+  .catch(err=>{console.log(err)})
+  }
 
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
@@ -55,17 +56,24 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll().then(([rows,fieldData])=>{
     res.render('admin/products', {
-      prods: products,
+      prods: rows,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
-  });
+  })
+  .catch(err=>{
+    console.log(err);
+  })
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.deleteById(prodId).then(([product])=>{
+    res.redirect('/admin/products');
+  })
+  .catch(err=>{
+    console.log(err);
+  })
 };
